39dcd32c0508c5a34c267b443764c014e0115165
Aleksander Gondek <gondekaleksander@hotmail.com>
2015-05-11 21:37:36 +0200

    Added AppSetings and Ninject

---
+++ CellAutomat/Data/AppSettingsHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace CellAutomat.Data
+{
+    public static class AppConfigHelper
+    {
+        public static String GetValueFromAppSettings(String key)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            if (!appSettings.HasKeys())
+            {
+                throw new ConfigurationErrorsException("App settings file contains no entries!");
+            }
+
+            var customSetting = appSettings[key];
+            if (null == customSetting)
+            {
+                throw new SettingsPropertyNotFoundException("There isn't setting with key: " + key);
+            }
+
+            return customSetting;
+        }
+    }
+}
